2bb2fb69a91299821179b62064829b8caf7f23d9
Gaurav Jain <136182348+GAURAVJAIN2498@users.noreply.github.com>
2025-09-24 12:02:51 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ const todoItems = [
   },
   {
     id: 2,
-    title: " No Study",
+    title: "Study",
     description: "Read Algebra and History textbook for the upcoming test",
     completed: false,
   },
